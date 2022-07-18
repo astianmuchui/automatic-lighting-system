16279070a6ae56c1ecd1c7fb9ac812622b506687
Sebastian Muchui <Sebastianmuchui79@gmail.com>
2022-07-18 12:04:27 +0300

    Initial build

---
+++ main.ino
@@ -0,0 +1,43 @@
+int pir = 1;
+int relay = 12;
+
+void setup(){
+	pinMode(pir,INPUT);
+  	pinMode(relay,OUTPUT);
+}
+
+void loop(){
+  if(detect_motion()){
+  	switchOnLights();
+  }else{
+  	SwitchOff();
+  }
+}
+
+bool detect_motion(){
+	int status = digitalRead(pir);
+  if(status == HIGH){
+  	return true;
+  }	else{
+  	return false;
+  }
+}
+
+void switchOnLights(){
+  	// check if lights were on
+  
+  if(digitalRead(relay) == LOW){
+	digitalWrite(relay,HIGH);
+  	// Turn on lights for 5 minutes
+  	delay(50000);  
+    SwitchOff();
+  }else{
+  	// Do nothing
+  }
+
+  	
+}
+
+void SwitchOff(){
+	digitalWrite(relay,LOW);
+}
